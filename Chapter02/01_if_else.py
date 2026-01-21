674a88b10696eb022f6163c573bc9706cd3b7036
BakkappaN <bakkappac@gmail.com>
2026-01-21 22:48:20 +0530

    Python Tutorials For Beginners

---
+++ Chapter02/01_if_else.py
@@ -0,0 +1,14 @@
+# Example1 - if & else
+marks = 30
+if marks < 35:
+    print('FAIL')
+else:
+    print('PASS')
+
+# Example2 - if, elif & else 
+if marks >= 90:
+    print('Grade A')
+elif marks >= 75:
+    print('Grade B')
+elif marks >= 50:
+    print('Grade C')

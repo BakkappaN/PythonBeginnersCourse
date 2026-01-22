6418a28d1abd9f7721cc08fb51e5bd56ef2488c8
BakkappaN <bakkappac@gmail.com>
2026-01-22 18:05:35 +0530

    Python Tutorials For Beginners

---
+++ Chapter02/04_while_loop.py
@@ -0,0 +1,12 @@
+# while loop
+
+# Iteration1, count=1
+# Iteration2, count=2
+# Iteration3, count=3
+# Iteration4, count=4
+# Iteration5, count=5
+# Iteration6, count=6, exit while loop
+count = 1
+while count <= 5:
+    print(count)
+    count+=1

804a5dfed3c4248f45bf320bf73021f1c732a486
BakkappaN <bakkappac@gmail.com>
2026-01-25 18:49:55 +0530

    Python Tutorials For Beginners

---
+++ Chapter03/05_lambda_functions.py
@@ -0,0 +1,8 @@
+# lambda functions
+# Example1 - Find out square of given number
+square = lambda x: x*x
+print(square(10))
+
+# Example2 - Find out sum of 2 no.
+addTwoNumbers = lambda num1, num2: num1 + num2
+print(addTwoNumbers(10, 20))
